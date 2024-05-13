fa71c0158d8a63f6ac2d3b4866013c54c285078b
zhim <wang.zhim@northeastern.edu>
2024-05-13 02:04:02 -0400

    add name and github link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,7 @@ export default function Labs(){
     return (
         <div>
             <h1>Labs</h1>
+            <span>Zhimin Wang</span>
             <TOC />
             <Routes>
                <Route path="/" element={<Navigate to="Lab1" />} />
