7938123b7ebf1902268bb1afc6566e70b83a0fe2
zhim <wang.zhim@northeastern.edu>
2024-06-16 16:08:51 -0400

    a6 fix profile

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "./Account";
+import Session from "./Account/Session";
 import Courses from "./Courses";
 import * as client from "./Courses/client";
 import Dashboard from "./Dashboard";
@@ -48,13 +49,14 @@ export default function Kanbas() {
           }, []);
   return (
     <Provider store={store}>
+      <Session>
     <div id="wd-kanbas" className="h-100 d-flex">
       <div className="d-none d-md-block bg-black h-100 position-fixed" >
         <KanbasNavigation />
       </div>
       <div className="flex-fill p-4" style={{ marginLeft: '120px' }}>
         <Routes>
-          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account/*" element={<Account />} />
           <Route path="Dashboard" element={<ProtectedRoute><Dashboard
           courses={courses}
@@ -67,16 +69,11 @@ export default function Kanbas() {
           <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses}/></ProtectedRoute>} />
           <Route path="Calendar" element={<h1>Calendar</h1>} />
           <Route path="Inbox" element={<h1>Inbox</h1>} />     
-          <Route path="Kanbas" element={<Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse} />} />
         </Routes>
       </div>
     </div>
+    </Session>
     </Provider>
+
   );
 }
